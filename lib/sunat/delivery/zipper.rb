cf3d55851e5b046b7702b619f1dd3f3672781cba
Laura Morillo-Velarde <laura.morillovelarde@gmail.com>
2015-03-25 12:49:15 +0100

    Adapted zipper to the new rubyzip version

--- lib/sunat/delivery/zipper.rb
+++ lib/sunat/delivery/zipper.rb
@@ -1,10 +1,10 @@
-require 'zip/zip'
+require 'zip'
 
 module SUNAT
   module Delivery
     class Zipper
       def zip(name, body)
-        zip = Zip::ZipOutputStream::write_buffer do |zip|
+        zip = Zip::OutputStream.write_buffer do |zip|
           zip.put_next_entry name
           zip.write body
         end
