171c26d10ce355e0916d673708c829740e8e1c45
Laura Morillo-Velarde <laura.morillovelarde@gmail.com>
2015-04-13 12:04:56 +0200

    Generated invoices and credit notes for case 2

--- homologation/generators/credit_note_generator.rb
+++ homologation/generators/credit_note_generator.rb
@@ -3,6 +3,22 @@ require_relative 'document_generator'
 class CreditNoteGenerator < DocumentGenerator
   def self.for_igv_invoice(group, group_case, associated_document, serie, pdf=false)
     line = associated_document.lines.first
+    credit_note = SUNAT::CreditNote.new(credit_note_data_for_line(line, associated_document, serie))
+    generate_documents(group, group_case, credit_note, pdf)
+    credit_note
+  end
+
+  def self.for_exempt_invoice(group, group_case, associated_document, serie, pdf=false)
+    exempt_line = associated_document.lines.last
+    
+    credit_note = SUNAT::CreditNote.new(credit_note_data_for_line(exempt_line, associated_document, serie))
+    generate_documents(group, group_case, credit_note, pdf)
+    credit_note
+  end
+
+  private 
+
+  def self.credit_note_data_for_line(line, associated_document, serie)
     legal_monetary_total = line.line_extension_amount.value + line.tax_totals.inject(0){|sum, tax| sum + tax.tax_amount.value}
     credit_note_data = {id: "#{serie}-#{"%03d" % @@document_serial_id}", customer: associated_document.customer,
                      billing_reference: {id: associated_document.id, document_type_code: TYPES[associated_document.class.name]},
@@ -12,8 +28,6 @@ class CreditNoteGenerator < DocumentGenerator
                      additional_monetary_totals: [{id: "1001", payable_amount: line.price}], tax_totals: line.tax_totals, 
                      legal_monetary_total: legal_monetary_total}
     @@document_serial_id += 1
-    credit_note = SUNAT::CreditNote.new(credit_note_data)
-    generate_documents(group, group_case, credit_note, pdf)
-    credit_note
+    credit_note_data  
   end
 end
--- homologation/generators/invoice_generator.rb
+++ homologation/generators/invoice_generator.rb
@@ -9,17 +9,12 @@ class InvoiceGenerator < DocumentGenerator
   end
 
   def self.exempt(group, group_case, items, serie, pdf=false)
-    invoice_data = {id: "#{serie}-#{"%03d" % @@document_serial_id}", customer: {legal_name: "Servicabinas S.A.", ruc: "20587896411"}, 
-                    tax_totals: [{amount: items*1800, type: :igv}], legal_monetary_total: 11800 * items, 
-                    additional_monetary_totals: [{id: "1001", payable_amount: 10000 * items}]}
-    @@document_serial_id += 1
-    lines = []
-    invoice_data[:lines] = (1..items).map do |item|
-      {id: item.to_s, quantity: 1, line_extension_amount: 10000, pricing_reference: 11800, price: 10000, tax_totals: [{amount: 1800, type: :igv}],
-        item: {id: item.to_s, description: "Item #{item}"}}
-    end
-    
+    invoice_data = data(serie, items - 1)
+    invoice_data[:lines] << {id: items.to_s, quantity: 1, line_extension_amount: 10000, pricing_reference: 10000, price: 10000,
+                             item: {id: items.to_s, description: "Item #{items}"}, tax_totals: [{amount: 0, type: :igv, code: "20"}], line_extension_amount: 10000}
+    invoice_data[:additional_monetary_totals] << {id: "1003", payable_amount: 10000}
     invoice = SUNAT::Invoice.new(invoice_data)
+    invoice.legal_monetary_total.value = invoice.legal_monetary_total.value + 10000
     generate_documents(group, group_case, invoice, pdf)
     invoice
   end
@@ -30,10 +25,12 @@ class InvoiceGenerator < DocumentGenerator
                     tax_totals: [{amount: items*1800, type: :igv}], legal_monetary_total: 11800 * items, 
                     additional_monetary_totals: [{id: "1001", payable_amount: 10000 * items}]}
       @@document_serial_id += 1
-      lines = []
-      invoice_data[:lines] = (1..items).map do |item|
-        {id: item.to_s, quantity: 1, line_extension_amount: 10000, pricing_reference: 11800, price: 10000, tax_totals: [{amount: 1800, type: :igv}],
-          item: {id: item.to_s, description: "Item #{item}"}}
+      invoice_data[:lines] = []
+      if items > 0
+        invoice_data[:lines] = (1..items).map do |item|
+          {id: item.to_s, quantity: 1, line_extension_amount: 10000, pricing_reference: 11800, price: 10000, tax_totals: [{amount: 1800, type: :igv}],
+            item: {id: item.to_s, description: "Item #{item}"}}
+        end
       end
       invoice_data
     end
