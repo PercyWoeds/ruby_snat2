27f0cbc23286eae9a37e4481cd097a81dc192404
Laura Morillo-Velarde <laura.morillovelarde@gmail.com>
2015-04-13 16:44:06 +0200

    Fixed reference id for credit notes.

--- homologation/generators/credit_note_generator.rb
+++ homologation/generators/credit_note_generator.rb
@@ -28,7 +28,7 @@ class CreditNoteGenerator < DocumentGenerator
     legal_monetary_total = line.line_extension_amount.value + line.tax_totals.inject(0){|sum, tax| sum + tax.tax_amount.value}
     credit_note_data = {id: "#{@serie}-#{"%03d" % @@document_serial_id}", customer: associated_document.customer,
                      billing_reference: {id: associated_document.id, document_type_code: TYPES[associated_document.class.name]},
-                     discrepancy_response: {reference_id: associated_document.id, response_code: "07", description: "Cliente no satisfecho"},
+                     discrepancy_response: {reference_id: "06", response_code: "07", description: "Cliente no satisfecho"},
                      lines: [{id: "1", quantity: line.quantity.quantity, unit: 'NIU', item: line.item,
                           price: line.price, pricing_reference: line.pricing_reference, tax_totals: line.tax_totals, line_extension_amount: line.line_extension_amount}],
                      additional_monetary_totals: [{id: "1001", payable_amount: line.price}], tax_totals: line.tax_totals, 
